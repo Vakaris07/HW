30f99c7690ebf43dbcaa9038b4b7b0c2238971b8
unknown <aleksandravicius.vakaris07@gmail.com>
2024-09-23 15:55:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++) {
+            System.out.println("Multiplication table for " +i);
+            for (int j=1; j<=10; j++) {
+                System.out.print(i + " * " + j + " = " + (i*j));
+                System.out.println();
+
+
+
+            }
+
+        }
+
+        }
+    }
